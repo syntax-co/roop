7657493ffffd8dcebb47989f430f8759f777ea55
syntax-co <munozchris484@gmail.com>
2025-03-04 16:46:46 -0600

    quick push

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
+  basePath: process.env.NODE_ENV === "production" ? "/roop" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/roop/" : "",
 };
 
 export default nextConfig;
