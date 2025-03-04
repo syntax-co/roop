655fb0d5f16a3391ddf7d4a09878138db8e43f58
syntax-co <munozchris484@gmail.com>
2025-03-04 14:41:24 -0600

    quick fix

--- src/components/home/hold-the-presses.js
+++ src/components/home/hold-the-presses.js
@@ -1,7 +1,6 @@
 import { useRouter } from "next/router";
 import { motion } from "framer-motion";
 import { useState } from "react";
-import Image from 'nextimage';
 
 
 const PressTile = ({title,image,path}) => {
